19635491cf9a36a4c14eba86dd4dc352f5f8b642
hthuang996 <hht2015ily@gmail.com>
2022-05-14 18:06:35 +0800

    Rust macro

---
+++ rust/macro/hello/src/main.rs
@@ -0,0 +1,15 @@
+use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
+struct Pancakes;
+
+// impl HelloMacro for Pancakes {
+//     fn hello_macro() {
+//         println!("Hello, Macro!");
+//     }
+// }
+
+fn main() {
+    Pancakes::hello_macro();
+}
---
+++ rust/macro/hello_macro/hello_macro_derive/src/lib.rs
@@ -0,0 +1,33 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+
+    impl_hello_macro(&ast)
+}
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote!{
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
